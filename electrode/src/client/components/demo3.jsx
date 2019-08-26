404aa645a4a0e98f9a9ca0d483285d97025bcf82
Barbara Choy <barbchoy@gmail.com>
2019-08-26 11:18:40 -0700

    add a React counter with CSS

--- electrode/src/client/components/demo3.jsx
+++ electrode/src/client/components/demo3.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Nav } from "./nav";
 import { connect } from "react-redux";
 import custom from "../styles/custom.css"; // eslint-disable-line no-unused-vars
-import demoStyle from "../styles/demo2.css"; // eslint-disable-line no-unused-vars
+import demo3 from "../styles/demo3.css"; // eslint-disable-line no-unused-vars
 
 export default class Demo3 extends Component {
   constructor(props) {
@@ -10,6 +10,58 @@ export default class Demo3 extends Component {
   }
 
   render() {
-    return <h1>Hi Electrode!</h1>;
+    return (
+      <div styleName={"custom.container"}>
+        <Nav {...this.props} />
+        <CounterParent />
+      </div>
+    );
+  }
+}
+
+class Counter extends React.Component {
+  render() {
+    return <div styleName={"demo3.text"}>{this.props.display}</div>;
+  }
+}
+
+class PlusButton extends React.Component {
+  render() {
+    return (
+      <button styleName={"demo3.button"} onClick={this.props.clickHandler}>
+        +
+      </button>
+    );
+  }
+}
+
+class CounterParent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.increase = this.increase.bind(this);
+  }
+
+  increase(e) {
+    var currentCount = this.state.count;
+    if (e.shiftKey) {
+      currentCount += 10;
+    } else {
+      currentCount += 1;
+    }
+    this.setState({
+      count: currentCount
+    });
+  }
+
+  render() {
+    return (
+      <div styleName={"demo3.background"}>
+        <Counter display={this.state.count} />
+        <PlusButton clickHandler={this.increase} />
+      </div>
+    );
   }
 }
